a7778851fa64ea07e14a07eb6e3e53251076063f
harshtadas8 <harshtadas8@gmail.com>
2026-01-08 10:20:39 +0530

    add3

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default function Courses() {
   const navigate = useNavigate();
   const { isAuthenticated, loginWithRedirect, getAccessTokenSilently } = useAuth0();
@@ -19,7 +19,7 @@ export default function Courses() {
       try {
         const token = await getAccessTokenSilently();
 
-        const res = await fetch("http://localhost:5000/api/courses/my", {
+        const res = await fetch(`${BASE_URL}/courses/my`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
